1b56bd90e5c9f115d54ae90fce680b9a15900d51
JesusC10 <jesus.gonzalez.ac@hotmail.com>
2023-05-29 10:10:21 -0600

    built model for pred glassdoor

--- ds_salary_proj/run.py
+++ ds_salary_proj/run.py
@@ -8,7 +8,7 @@ Created on Sat May 13 19:45:07 2023
 import glassdoor_scrapper as gs
 
 path = "chromedriver"
-keyword = "data scientist"
+keyword = "data science"
 
 #This line will open a new chrome window and start the scraping.
 df = gs.get_jobs("data scientist", 30, True, path, 15)
